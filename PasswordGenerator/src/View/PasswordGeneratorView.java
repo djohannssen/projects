21be5f44a3d24ce87399e4cbd72c2f557b9548c4
djohannssen <dirk.johannssen@ppi.de>
2015-10-18 23:56:22 +0200

    Application cenetered and clipboard-copying implemented.

--- PasswordGenerator/src/View/PasswordGeneratorView.java
+++ PasswordGenerator/src/View/PasswordGeneratorView.java
@@ -1,8 +1,14 @@
 package View;
 
+import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,37 +17,48 @@ import javax.swing.JTextField;
 
 import Controller.PasswordGeneratorController;
 
-public class PasswordGeneratorView extends JFrame implements ActionListener{
-	
+public class PasswordGeneratorView extends JFrame implements ActionListener {
+
 	/**
 	 * Default Id.
 	 */
 	private static final long serialVersionUID = 1L;
 
 	private String password;
-	
+
 	private PasswordGeneratorController controller;
-	
+
 	JButton button;
 	JTextField textArea;
 	JPanel panel;
-	
-	public void setUpFrame(){
+
+	public void setUpFrame() {
 		this.setTitle("PasswordGenerator");
 		this.setSize(400, 100);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		panel = new JPanel();
 		panel.setLayout(new GridLayout(0, 1));
 		textArea = new JTextField("A Textarea");
+		textArea.addMouseListener(new MouseAdapter() {
+			 @Override
+			  public void mouseClicked(MouseEvent e) {
+				 copy(textArea.getText());
+				 System.out.println(textArea.getText());
+			    textArea.setText("Passwort in Zwischenablage");
+			  }
+			});
+
 		button = new JButton("Generieren");
 		button.addActionListener(this);
 		panel.add(button);
 		panel.add(textArea);
+
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation((dim.width - this.getWidth()) / 2, (dim.height - this.getHeight()) / 2);
+
 		this.add(panel);
 		this.setVisible(true);
 	}
-
-
 	public void repaint() {
 		textArea.setText(password);
 		panel.repaint();
@@ -52,7 +69,20 @@ public class PasswordGeneratorView extends JFrame implements ActionListener{
 	public void actionPerformed(ActionEvent actionEvent) {
 		controller.viewPasswordRequest();
 	}
-	
+
+	public static void copy(String text) {
+		Clipboard clipboard = getSystemClipboard();
+
+		clipboard.setContents(new StringSelection(text), null);
+	}
+
+	private static Clipboard getSystemClipboard() {
+		Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+		Clipboard systemClipboard = defaultToolkit.getSystemClipboard();
+
+		return systemClipboard;
+	}
+
 	public String getPassword() {
 		return password;
 	}
@@ -61,11 +91,9 @@ public class PasswordGeneratorView extends JFrame implements ActionListener{
 		this.password = password;
 	}
 
-
 	public PasswordGeneratorController getController() {
 		return controller;
 	}
-
 
 	public void setController(PasswordGeneratorController controller) {
 		this.controller = controller;
